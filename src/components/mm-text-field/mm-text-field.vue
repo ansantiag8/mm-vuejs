61d01204d66b421d16c6353a0949d837d34b55c0
Santiago Rodriguez <an.santiago@tutanota.com>
2018-08-26 09:11:16 -0500

    se adiciona el manejo de eventos

--- src/components/mm-text-field/mm-text-field.vue
+++ src/components/mm-text-field/mm-text-field.vue
@@ -1,16 +1,58 @@
 <template>
   <div class="mm-text-field">
-    <input type="text">
-    <label for="">Campo de texto</label>
+    <input
+      v-bind:class="{
+        'mm-input-error': classInput.mmInputError,
+        'mm-input-not-empty': classInput.mmInputNotEmpty,
+        'mm-input-touched': classInput.mmInputTouched,
+      }"
+      v-bind:type="type"
+      v-on:blur="eventBlur"
+      v-on:focus="eventFocus"
+      v-on:keyup="eventKeyup"
+      :required="required"
+    >
+    <label for="">{{ label }}</label>
     <span>Helper text</span>
   </div>
 </template>
 
 <script>
-  import 'mm-text-field/src/styles/mm-text-field.css';
   import mmTextField from 'mm-text-field';
 
+  import eventBlur from './events/eventBlur'
+  import eventFocus from './events/eventFocus'
+  import eventKeyup from './events/eventKeyup'
+
   export default {
+    data: function () {
+      return {
+        classInput: {
+          mmInputError: false,
+          mmInputNotEmpty: false,
+          mmInputTouched: false,
+        }
+      };
+    },
+    methods: {
+      eventBlur,
+      eventFocus,
+      eventKeyup,
+    },
+    props: {
+      label: {
+        type: String,
+        default: 'Campo',
+      },
+      required: {
+        type: Boolean,
+        default: false,
+      },
+      type: {
+        type: String,
+        default: 'text',
+      }
+    },
     mounted: function () {
       mmTextField();
     }
